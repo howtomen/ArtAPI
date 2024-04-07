781e10a754616d1d43a4342e89b7b01e0c63dc41
Daniel Ariza <danielariza@csu.fullerton.edu>
2024-04-07 18:06:12 -0400

    remove old code

--- internal/DB/db.go
+++ internal/DB/db.go
@@ -33,57 +33,9 @@ func NewDbConnection() (*Database, error) {
 	return &Database{
 		Client: db,
 	}, nil 
-
-	// //check if table exists, if not create table.
-	// exists, err := tableExists(db, "art_vault")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// if !exists {
-	// 	err = createTable(db)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// }
-	// return db
 }
 
 //Adding ping function so that later we can health check the conn to DB easily
 func (d *Database) Ping(ctx context.Context) error {
 	return d.Client.DB.PingContext(ctx)
 }
-
-// func tableExists(db *sqlx.DB, table string) (bool, error) {
-// 	query := fmt.Sprintf(`
-//         SELECT EXISTS (
-//             SELECT FROM information_schema.tables 
-//             WHERE  table_schema = 'public'
-//             AND    table_name   = '%s'
-//         );
-//     `, table)
-// 	var exists bool 
-// 	err := db.QueryRow(query).Scan(&exists)
-// 	if err != nil {
-// 		return false, err
-// 	}
-// 	return exists, nil
-// }
-
-// func createTable(db *sqlx.DB) error{
-// 	sqlLines, err := readSQLFile("create_db.sql")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	_,err = db.Exec(sqlLines)
-// 	return err
-// }
-
-// func readSQLFile(file string) (string, error) {
-// 	data, err := os.ReadFile(file)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	return string(data), err
-// }
