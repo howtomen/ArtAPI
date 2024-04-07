5e264a1199c32ab8ab5910da2598fda5be019a92
Daniel Ariza <danielariza@csu.fullerton.edu>
2024-04-07 18:04:25 -0400

    implement a graceful shutdown

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,8 +1,13 @@
 package http
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -38,8 +43,22 @@ func (h *Handler) mapRoutes () {
 }
 
 func (h *Handler) Serve() error {
-	if err := h.Server.ListenAndServe(); err != nil {
-		return err
-	}
+	go func() {
+		if err := h.Server.ListenAndServe(); err != nil {
+			log.Println(err.Error())
+		}
+	}()
+	
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	// this should handle shutdown but by default waits indefinitely
+	// we defer cancel so that it doesnt hang 
+	defer cancel() 
+	h.Server.Shutdown(ctx)
+
+	log.Println("shut down gracefully")
 	return nil 
 }
