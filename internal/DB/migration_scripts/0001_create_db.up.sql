53c3ac72207731cfff6be227fdd30b701fefe6b5
Daniel Ariza <danielariza@csu.fullerton.edu>
2024-04-11 09:54:13 -0400

    add new table to beign implementing search

--- internal/DB/migration_scripts/0001_create_db.up.sql
+++ internal/DB/migration_scripts/0001_create_db.up.sql
@@ -1,6 +1,3 @@
--- naming 0001 since the migration library uses that 
--- naming convention to dictate execution order
--- this is also why there is a .up and .down
 CREATE TABLE IF NOT EXISTS art_vault
 (
   "id" uuid PRIMARY KEY,
@@ -18,3 +15,30 @@ CREATE TABLE IF NOT EXISTS art_vault
   "country" text
 );
 
+-- CREATE TABLE THAT WILL ALLOW US TO SEARCH RECORDS 
+CREATE TABLE IF NOT EXISTS search_idx
+(
+  id uuid PRIMARY KEY NOT NULL,
+  search tsvector NOT NULL
+);
+
+CREATE FUNCTION update_search_idx() RETURNS TRIGGER AS $$
+BEGIN
+  INSERT INTO search_idx (id, search)
+    SELECT NEW.id, to_tsvector('simple', NEW.accession_year) || ' ' ||
+                   to_tsvector('simple', NEW.department) || ' ' ||
+                   to_tsvector('simple', NEW.title) || ' ' ||
+                   to_tsvector('simple', NEW.object_name) || ' ' ||
+                   to_tsvector('simple', NEW.culture) || ' ' ||
+                   to_tsvector('simple', NEW.period) || ' ' ||
+                   to_tsvector('simple', NEW.artist_display_name) || ' ' ||
+                   to_tsvector('simple', NEW.city) || ' ' ||
+                   to_tsvector('simple', NEW.country);
+  RETURN NEW;
+END; 
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER art_row_added
+AFTER INSERT ON art_vault
+FOR EACH ROW
+EXECUTE FUNCTION update_search_idx();
